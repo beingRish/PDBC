be63c069b43e1787de75dc82b94e1c4e922813f4
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-18 18:33:19 +0530

    Create table using python

--- example.py
+++ example.py
@@ -9,7 +9,8 @@ try:
         user = os.getenv('MYSQL_USER'),
         password = os.getenv('MYSQL_PASSWORD'),
         host = os.getenv('MYSQL_HOST'),
-        port = int(os.getenv('MYSQL_PORT'))
+        port = int(os.getenv('MYSQL_PORT')),
+        database = os.getenv('DATABASE')
     )
     if conn.is_connected():
         print("Connected")
@@ -18,10 +19,22 @@ except Exception as e:
 
 cur = conn.cursor()
 # cur.execute("CREATE DATABASE Codeyug")
-cur.execute("SHOW DATABASES")
+# cur.execute("SHOW DATABASES")
+# for data in cur:
+#     print(data[0])
+# cur.execute("USE codeyug")
+cur.execute("""
+        CREATE TABLE IF NOT EXISTS tutorial1(
+        Video_id INT PRIMARY KEY,
+        Video_name VARCHAR(100) NOT NULL,
+        Video_views INT,
+        Watchtime FLOAT
+    )
+""")
+
+cur.execute("DESC tutorial")
 for data in cur:
     print(data[0])
-
 
 
 cur.close()
---
+++ queries.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tutorial(
+    Video_id INT PRIMARY KEY,
+    Video_name VARCHAR(100) NOT NULL,
+    Video_views INT,
+    Watchtime FLOAT
+)
+
+DESC tutorial
