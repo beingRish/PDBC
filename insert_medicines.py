e6dbcdfa25c79fa4a387bcab6d1b278c16b98e4b
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-19 01:50:11 +0530

    Execute parameterized query

--- insert_medicines.py
+++ insert_medicines.py
@@ -2,23 +2,35 @@ from db_connect import get_connection
 
 db_connection = get_connection()
 if db_connection:
-    cur = db_connection.cursor(prepared=True)
+    cur = db_connection.cursor()
     try:
         sql = """INSERT INTO medicines
             (drug_id, drug_name, price, expiry)
-            VALUES (%s, %s, %s, %s)
+            VALUES (%(drug_id)s, %(drug_name)s, %(price)s, %(expiry_date)s)
         """
 
-        n = int(input("Enter number of medicines:"))
-        for i in range(n):
+        while True:
             drug_id = int(input("Enter drug id:"))
             drug_name = input("Enter drug name:")
             price = float(input("Enter price:"))
-            expiry_date = input("Enter expiry date(2025-07-09):")
+            expiry_date = input("Enter expiry date(YYYY-MM-DD):")
 
-            cur.execute(sql,(drug_id, drug_name, price, expiry_date))
+            cur.execute(
+                sql,
+                {
+                    'drug_id': drug_id,
+                    'drug_name': drug_name,
+                    'price': price,
+                    'expiry_date': expiry_date
+                }
+            )
             db_connection.commit()
+            ans = input("Do you have more medicines:(y/n):").lower()
+            if ans!='y':
+                break
             print(f"{cur.rowcount} row(s) inserted")
+            print("-"*50)
+        
     except Exception as e:
         db_connection.rollback()
         print("Insert failed:", e)
