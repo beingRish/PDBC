65589928945f4eff874d986a4981d73a3b959d31
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-18 19:01:36 +0530

    Insert Record into MySQL Table using Python Script

--- example.py
+++ example.py
@@ -23,19 +23,32 @@ cur = conn.cursor()
 # for data in cur:
 #     print(data[0])
 # cur.execute("USE codeyug")
-cur.execute("""
-        CREATE TABLE IF NOT EXISTS tutorial1(
-        Video_id INT PRIMARY KEY,
-        Video_name VARCHAR(100) NOT NULL,
-        Video_views INT,
-        Watchtime FLOAT
-    )
-""")
+# cur.execute("""
+#         CREATE TABLE IF NOT EXISTS tutorial1(
+#         Video_id INT PRIMARY KEY,
+#         Video_name VARCHAR(100) NOT NULL,
+#         Video_views INT,
+#         Watchtime FLOAT
+#     )
+# """)
 
-cur.execute("DESC tutorial")
-for data in cur:
-    print(data[0])
+# cur.execute("DESC tutorial")
+# for data in cur:
+#     print(data[0])
 
-
+sql = """
+INSERT INTO tutorial
+(Video_id, Video_name, Video_views, Watchtime) VALUES
+(104, 'python basics', 25000, 20.0),
+(105, 'exception basics', 12000, 30.4);
+"""
+try:
+    cur.execute(sql)
+    conn.commit()
+    print("Successfully data inserted")
+    print(f"{cur.rowcount} rows inserted")
+except Exception as e:
+    conn.rollback()
+    print("Something went wrong", e)
 cur.close()
 conn.close()
--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE Codeyug
+
+SHOW DATABASES
+
 CREATE TABLE IF NOT EXISTS tutorial(
     Video_id INT PRIMARY KEY,
     Video_name VARCHAR(100) NOT NULL,
@@ -6,3 +10,8 @@ CREATE TABLE IF NOT EXISTS tutorial(
 )
 
 DESC tutorial
+
+
+INSERT INTO tutorial
+(Video_id, Video_name, Video_views Watchtime) VALUES
+(101, 'oop basics', 15000, 100);
